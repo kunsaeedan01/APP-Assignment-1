fc3cb46f70062dace6df1f3a455ef4cd218cfc20
kunsaeedan01 <77623559+kunsaeedan01@users.noreply.github.com>
2021-09-26 23:10:29 +0600

    Update main.py

--- src/main.py
+++ src/main.py
@@ -10,14 +10,13 @@ list_of_crypto = []
 for key in top_100:
     list_of_crypto.append(key)
 
-order = 1
-user_input = int(input("Print the number of cryptocurrencies for output:  "))
 
-if user_input > len(list_of_crypto) or user_input < 1:
-    print("Your number is out of range")
-else:
-    for coin in range(0, user_input):
-        print(f"{order}. {list_of_crypto[coin]}")
-        order += 1
-
-
+def crypto_list():
+    user_input = int(input("Print the number of cryptocurrencies for output:  "))
+    order = 1
+    if user_input > len(list_of_crypto) or user_input < 1:
+        print("Your number is out of range")
+    else:
+        for coin in range(0, user_input):
+            print(f"{order}. {list_of_crypto[coin]}")
+            order += 1
